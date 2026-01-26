b456f278939733165c56e4ace35e177be3f9e04f
L-Surkov <ilya.surkov11@gmail.com>
2026-01-26 12:00:45 +0300

    Update remote properties

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -10,10 +10,10 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Attach {
 
-    @Attachment(value = "{attachName}", type = "image/png")
+   /* @Attachment(value = "{attachName}", type = "image/png")
     public static byte[] screenshotAs(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
-    }
+    }*/
 
     @Attachment(value = "Page source", type = "text/plain")
     public static byte[] pageSource() {
